76df50ffe6c8e09dd08d64d62343842bd42d2673
Beth Wilson <bethwilson777@gmail.com>
2022-11-08 20:28:36 -0700

    Fetched api data for destinations to Destinations class file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Destinations from './Destinations/Destinations';
 
 function App() {
   return (
     <div className="App">
+      <h1>SUP Colorado</h1>
+      <Destinations />
     </div>
   );
 }
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,5 @@
 const fetchDestinations = () => {
     return fetch('https://sup-colorado-api.vercel.app/api/v1/destinations')
-    .then(response => {
-      if (!response.ok) {
-          throw new Error('Not a 200 status')
-      }
-      return response.json()
-    })
   };
 
 export { fetchDestinations }
