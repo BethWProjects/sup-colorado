879087396536f5be3d89bca533c289de78e1629e
Beth Wilson <bethwilson777@gmail.com>
2022-11-10 14:02:25 -0700

    Added css for details page, including responsive updates

--- src/SUPDetails/SUPdetails.js
+++ src/SUPDetails/SUPdetails.js
@@ -1,64 +1,72 @@
-import React, { Component } from 'react'
-import { fetchDestinations } from '../apiCalls'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import './SUPDetails.css'
+import React, { Component } from "react";
+import { fetchDestinations } from "../apiCalls";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./SUPDetails.css";
 
 class SUPDetails extends Component {
-    constructor() {
-        super()
-        this.state = {
-            destination: {}
-        }
+  constructor() {
+    super();
+    this.state = {
+      destination: {},
+    };
+  }
+
+  componentDidMount = async () => {
+    try {
+      const destList = await fetchDestinations();
+      const data = await destList.json();
+      const destMatch = await data.destination.find(
+        (dest) => dest.id === Number(this.props.destId)
+      );
+      this.setState({ destination: destMatch, loading: false });
+    } catch (error) {
+      this.setState({
+        error:
+          "Sorry, no paddle locations available.  Please try again another time!",
+      });
     }
+  };
 
-    componentDidMount = async () => {
-        try { const destList = await fetchDestinations();
-            const data = await destList.json();
-            const destMatch = await data.destination.find(dest => dest.id === Number(this.props.destId));
-            this.setState({ destination: destMatch, loading: false })   
-        } catch (error) {
-            this.setState({ error: "Sorry, no paddle locations available.  Please try again another time!"})
-        }
-    }
-
-
-
-    render() {
-        const dest = this.state.destination;
-        return (
-            <div className='details'>
-            <div className='dest-details-container'>
-                <div className='image-container'>
-                    <Link to={'/'}>
-                        <button className='details-home-button'>Home</button>
-                    </Link>
-                </div>
-                <div className='details-container'>
-                    <div className='details-section'>
-                        <h3 className='details-title'>{dest.title}</h3>
-                        <p className='details-type'>Type - {dest.type}</p>
-                        <p className='details-pets'>Pets Allowed? - {dest.petsAllowed}</p>
-                        <p className='details-boats'>Motorboats Allowed? - {dest.motorBoatsAllowed}</p>
-                        <p className='details-cost'>Cost - {dest.cost}</p>
-                        <p className='details-location'>Location - {dest.location}</p>
-                        <p className='details-drive'>Drive time from Denver {dest.driveTimeFromDenver}</p>
-                    </div>
-                    <div className='overview-container'>
-                        <p className='details-overview'>{dest.overview}</p>
-                    </div>
-                </div>
-               
+  render() {
+    const dest = this.state.destination;
+    return (
+      <div className="details">
+        <div className="dest-details-container">
+          <div className="image-container">
+            <Link to={"/"}>
+              <button className="details-home-button">Home</button>
+            </Link>
+          </div>
+          <div className="details-container">
+            <div className="details-section">
+              <h3 className="details-title">{dest.title}</h3>
+              <p className="details-location"><strong>Location</strong> - {dest.location}</p>
+              <p className="details-drive">
+                <strong>Drive time from Denver</strong> - {dest.driveTimeFromDenver}
+              </p>
+              <p className="details-cost"><strong>Cost</strong> - {dest.cost}</p>
+              <p className="details-type"><strong>Lake/River?</strong> - {dest.type}</p>
+              <p className="details-pets"><strong>Pets Allowed?</strong> - {dest.petsAllowed}</p>
+              <p className="details-boats">
+                <strong>Motorboats Allowed?</strong> - {dest.motorBoatsAllowed}
+              </p>
             </div>
-                <img className='details-image' src={dest.image}/>
+            <div className="overview-container">
+              <p className="details-overview">{dest.overview}</p>
             </div>
-        )
-
-    }
+          </div>
+        </div>
+        <div className="image-container">
+          <img className="details-image" src={dest.image} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default SUPDetails
+export default SUPDetails;
 
 SUPDetails.propTypes = {
-    destId: PropTypes.string
-}
+  destId: PropTypes.string,
+};
